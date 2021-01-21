a11fb940a567a615c365a055268b758c6b122d5c
LeoDoesCode <57913577+LeoDoesCode@users.noreply.github.com>
2021-01-21 13:16:46 +0000

    Add files via upload

---
+++ App_Data/User.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table] 
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(100) NULL, 
+    [IsAdmin] BIT NULL, 
+    [Password] NVARCHAR(50) NULL, 
+    [Email] NVARCHAR(50) NULL, 
+    [Born] DATE NULL, 
+    [Intro] NVARCHAR(500) NULL
+
+)
